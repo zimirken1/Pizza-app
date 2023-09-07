e8126464161b1cd9887ad834abf6aa2238e819c1
Alex <105302357+zimirken1@users.noreply.github.com>
2023-09-07 19:32:06 +0300

    Added about page information, styles

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -7,13 +7,22 @@ const About = () => {
             <div className={"offer-section"}>
                 <div className={"offer-section__image"}></div>
             </div>
-            <div className={"info-section"}>
-                <div className={"info-section__title"}>
-                    <div className={"title__text"}>
-                        О нас
+            <div className={"history-section"}>
+                <div className={"history-article"}>
+                    <div className={"title"}>Немного нашей истории</div>
+                    <div className={"img"}></div>
+                    <div className={"text"}>
+                        История "Pizzeria" - это история времен, когда качество было важнее количества,
+                        и любовь к еде собирала людей вместе. Здесь каждая пицца готовится с любовью и тщательностью,
+                        чтобы побаловать гостей неповторимым вкусом и атмосферой итальянского гостеприимства.
+                        Если вы хотите окунуться в атмосферу старой Италии и
+                        насладиться волшебством пиццы, "Pizzeria" всегда открыта для вас.
                     </div>
                 </div>
             </div>
+            <footer className={"footer"}>
+
+            </footer>
         </>
     );
 };
